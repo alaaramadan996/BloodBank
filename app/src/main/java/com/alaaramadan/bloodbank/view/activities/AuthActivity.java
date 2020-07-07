33acc741efdacf93c2fc4a38119c11ea09a12923
Alaa <alaaramadan996@gmail.com>
2020-07-07 22:10:36 +0200

    Initial commit

---
+++ app/src/main/java/com/alaaramadan/bloodbank/view/activities/AuthActivity.java
@@ -0,0 +1,16 @@
+package com.alaaramadan.bloodbank.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alaaramadan.bloodbank.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
---
+++ app/src/main/java/com/alaaramadan/bloodbank/view/activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.alaaramadan.bloodbank.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alaaramadan.bloodbank.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
